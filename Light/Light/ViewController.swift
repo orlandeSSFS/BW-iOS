9d828d0c366f467d01600f69c067fa6d8bf11607
Benjamin Levy <levybe@ssfs.org>
2022-02-02 11:54:57 -0500

    Started working on Light.

---
+++ Light/Light/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Benjamin Ethan Levy on 2/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
