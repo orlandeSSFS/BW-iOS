bd0dc940a8094cbadcf3cb74825736a195951861
Benjamin Levy <levybe@ssfs.org>
2022-02-03 13:53:29 -0500

    Worked on Light application.

--- Light/Light/ViewController.swift
+++ Light/Light/ViewController.swift
@@ -13,7 +13,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+    }
+    
+    
 
 }
 
