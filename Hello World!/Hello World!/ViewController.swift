6efc93e1b013128cabc1a9a5d632c41a0b4b8403
Benjamin Levy <levybe@ssfs.org>
2022-02-09 09:55:27 -0500

    Took website text.

--- Hello World!/Hello World!/ViewController.swift
+++ Hello World!/Hello World!/ViewController.swift
@@ -10,14 +10,25 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var helloLabel: UILabel!
     
+    
+    @IBOutlet weak var webText: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        if let url = URL(string: "https://www.ssfs.org") {
+            do {
+                let contents = try String(contentsOf: url)
+                webText.text = contents
+            } catch {
+                // contents could not be loaded
+            }
+        } else {
+            // The URL was bad!
+        }
         // Do any additional setup after loading the view.
     }
     @IBAction func changeText(_ sender: UIButton) {
         helloLabel.text = "This is me!"
     }
     
-
 }
 
