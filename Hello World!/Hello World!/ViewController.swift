c48344986d4ba3514cea5d5173c32ffcbc7135a5
Benjamin Levy <levybe@ssfs.org>
2022-02-10 11:00:03 -0500

    Tried to create a picker.

--- Hello World!/Hello World!/ViewController.swift
+++ Hello World!/Hello World!/ViewController.swift
@@ -7,9 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     @IBOutlet weak var helloLabel: UILabel!
     
+    
+    @IBOutlet weak var picker: UIPickerView!
+    var pickerData: [String] = [String]()
     
     @IBOutlet weak var webText: UITextView!
     override func viewDidLoad() {
@@ -25,10 +28,32 @@ class ViewController: UIViewController {
             // The URL was bad!
         }
         // Do any additional setup after loading the view.
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        
+        pickerData = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"]
     }
     @IBAction func changeText(_ sender: UIButton) {
         helloLabel.text = "This is me!"
     }
-    
+    override func didReceiveMemoryWarning() {
+          super.didReceiveMemoryWarning()
+          // Dispose of any resources that can be recreated.
+      }
+
+      // Number of columns of data
+      func numberOfComponents(in pickerView: UIPickerView) -&gt; Int {
+          return 1
+      }
+      
+      // The number of rows of data
+      func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -&gt; Int {
+          return pickerData.count
+      }
+      
+      // The data to return fopr the row and component (column) that's being passed in
+      func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -&gt; String? {
+          return pickerData[row]
+      }
 }
 
