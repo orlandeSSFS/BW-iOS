c71efdff7d39531113d08db460de76eb5b3defcc
Benjamin Levy <levybe@ssfs.org>
2022-02-10 10:47:09 -0500

    Finished Function lab.

--- 2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/2 - Functions/Lab - Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
@@ -3,11 +3,28 @@
 
  Write a function called `introduceMyself` that prints a brief introduction of yourself. Call the function and observe the printout.
  */
-
-
+func introduceMyself() {
+    print("My name is Benjamin Levy. I am a 17-year old pursuing a career in computer science or game design.")
+}
+introduceMyself()
 //:  Write a function called `magicEightBall` that generates a random number and then uses either a switch statement or if-else-if statements to print different responses based on the random number generated. `let randomNum = Int.random(in: 0...4)` will generate a random number from 0 to 4, after which you can print different phrases corresponding to the number generated. Call the function multiple times and observe the different printouts.
 import Foundation
-
+func magicEightBall() {
+    let randomNum = Int.random(in: 0...4)
+    switch (randomNum) {
+    case 1:
+        print("You called 1!")
+    case 2:
+        print("You called 2!")
+    case 3:
+        print("You called 3!")
+    case 4:
+        print("You called 4!")
+    default:
+        print("You called 0!")
+    }
+}
+magicEightBall()
 
 /*:
 page 1 of 6  |  [Next: App Exercise - A Functioning App](@next)
