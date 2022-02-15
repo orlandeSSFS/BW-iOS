a7b8c801edc6cb5fcb653d69750ac31fea84620f
Benjamin Levy <levybe@ssfs.org>
2022-02-15 15:15:00 -0500

    Worked on Structures more.

--- 2 - Introduction to UIKit/3 - Structures/Lab - Structures.playground/Pages/6. App Exercise - Workout Functions.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/3 - Structures/Lab - Structures.playground/Pages/6. App Exercise - Workout Functions.xcplaygroundpage/Contents.swift
@@ -15,15 +15,22 @@ struct RunningWorkout {
     }
 }
 
-
+var workout = RunningWorkout(distance: 10.0, time: 5.2, elevation: 20.7)
+workout.postWorkoutStats()
 //:  A `Steps` struct has been created for you below, representing the day's step-tracking data. It has the goal number of steps for the day and the number of steps taken so far. Create a method on `Steps` called `takeStep` that increments the value of `steps` by one. Then create an instance of `Steps` and call `takeStep()`. Print the value of the instance's `steps` property before and after the method call.
 struct Steps {
     var steps: Int
     var goal: Int
     
+     mutating func takeStep() {
+        steps += 1
+    }
 }
 
-
+var stepper = Steps(steps: 12, goal: 13)
+print(stepper.steps)
+stepper.takeStep()
+print(stepper.steps)
 /*:
 [Previous](@previous)  |  page 6 of 10  |  [Next: Exercise - Computed Properties and Property Observers](@next)
  */
--- 2 - Introduction to UIKit/3 - Structures/Lab - Structures.playground/Pages/9. Exercise - Type Properties and Methods.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/3 - Structures/Lab - Structures.playground/Pages/9. Exercise - Type Properties and Methods.xcplaygroundpage/Contents.swift
@@ -3,14 +3,24 @@
  
  Imagine you have an app that requires the user to log in. You may have a `User` struct similar to that shown below. However, in addition to keeping track of specific user information, you might want to have a way of knowing who the current logged in user is. Create a `currentUser` type property on the `User` struct below and assign it to a `user` object representing you. Now you can access the current user through the `User` struct. Print out the properties of `currentUser`.
  */
+
+import Darwin
 struct User {
         
     var userName: String
     var email: String
     var age: Int
+    
+    static var currentUser: User = User(userName: "Benjamin", email: "levybe@ssfs.org", age: 17)
+    
+    func logIn(user: User) {
+        
+    }
 }
 
-
+print(User.currentUser.userName)
+print(User.currentUser.email)
+print(User.currentUser.age)
 /*:
  There are other properties and actions associated with a `User` struct that might be good candidates for a type property or method. One might be a method for logging in. Go back and create a type method called `logIn(user:)` where `user` is of type `User`. In the body of the method, assign the passed in user to the `currentUser` property, and print out a statement using the user's userName saying that the user has logged in.
  
