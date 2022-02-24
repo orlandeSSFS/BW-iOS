9c9e774b630d9d8e998e90cf14bcb45399448d80
Benjamin Levy <levybe@ssfs.org>
2022-02-24 12:34:37 -0500

    Worked on Collections.

--- 2 - Introduction to UIKit/5 - Collections/Lab - Collections.playground/Pages/2. App Exercise - Activity Challenge.xcplaygroundpage/Contents.swift
+++ 2 - Introduction to UIKit/5 - Collections/Lab - Collections.playground/Pages/2. App Exercise - Activity Challenge.xcplaygroundpage/Contents.swift
@@ -7,19 +7,20 @@
  
  Using arrays of type `String`, create at least two lists, one for walking challenges, and one for running challenges. Each should have at least two challenges and should be initialized using an array literal. Feel free to create more lists for different activities.
  */
-
-
+var challenges = [["Walk 3 miles a day", "Walk 5 times a week"], ["Run 6 miles a day", "Run 5 times a week"]]
 //:  In your app you want to show all of these lists on the same screen grouped into sections. Create a `challenges` array that holds each of the lists you have created (it will be an array of arrays). Using `challenges`, print the first element in the second challenge list.
-
+print(challenges[1][0])
 
 //:  All of the challenges will reset at the end of the month. Use the `removeAll` to remove everything from `challenges`. Print `challenges`.
-
-
+challenges.removeAll()
+print(challenges)
 //:  Create a new array of type `String` that will represent challenges a user has committed to instead of available challenges. It can be an empty array or have a few items in it.
-
+var activeChallenges: [String]
 
 //:  Write an if statement that will use `isEmpty` to check if there is anything in the array. If there is not, print a statement asking the user to commit to a challenge. Add an else-if statement that will print "The challenge you have chosen is <INSERT CHOSEN CHALLENGE>" if the array count is exactly 1. Then add an else statement that will print "You have chosen multiple challenges."
-
+if (activeChallenges.isEmpty) {
+    print("Please commit to a challenge")
+}
 
 /*:
 [Previous](@previous)  |  page 2 of 4  |  [Next: Exercise - Dictionaries](@next)
