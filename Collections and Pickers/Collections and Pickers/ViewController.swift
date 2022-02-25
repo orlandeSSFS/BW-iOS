faff67be448970f37c0bf43a8c28d3c900ff9401
Benjamin Levy <levybe@ssfs.org>
2022-02-25 11:54:43 -0500

    Started work on Collections and Pickers.

---
+++ Collections and Pickers/Collections and Pickers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Collections and Pickers
+//
+//  Created by Benjamin Ethan Levy on 2/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    var classes = [""]
+
+
+}
+
